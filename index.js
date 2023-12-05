8810440ce6ec298ae079a372cfbd8ba14abe82ec
Tomás Beckmann <147322170+tomasbeckmann@users.noreply.github.com>
2023-12-05 19:02:14 -0300

    Uploaded files.

---
+++ index.js
@@ -0,0 +1,39 @@
+const card = document.getElementById("card")
+const iconTop = document.querySelector(".icon1");
+const number = document.querySelector(".number");
+const iconBot = document.querySelector(".icon2");
+
+function actionCardNumber() {
+
+    let numbersArray = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+
+    let generateNumber = Math.floor(Math.random() * 13);
+
+    number.textContent = numbersArray[generateNumber];
+}
+
+function actionIcon() {
+
+    let iconString = ["♦","♥","♠","♣"];
+
+    let generateNumber = Math.floor(Math.random() * 4);
+
+    iconTop.textContent = iconString[generateNumber];
+    iconBot.textContent = iconString[generateNumber];
+
+    if(iconString[generateNumber] == "♥" || iconString[generateNumber] == "♦"){
+        iconTop.style.color = "red";
+        iconBot.style.color = "red";
+    } else {
+        iconTop.style.color = "black";
+        iconBot.style.color = "black";
+    }
+
+}
+
+card.addEventListener("click", actionCardNumber)
+card.addEventListener("click", actionIcon)
+
+card.addEventListener("load", actionCardNumber())
+card.addEventListener("load", actionIcon())
+
